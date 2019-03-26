d69ca77827196436fb01d0c4cde21e25e1946336
mihaly044 <mihaly044@gmail.com>
2019-03-26 18:14:59 +0100

    Cleanup

--- injector/injector.cpp
+++ injector/injector.cpp
@@ -1,17 +1,17 @@
 // injector.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
 #include <iostream>
 #include <Windows.h>
-#include <TlHelp32.h>
-#include <Shlwapi.h>
-#include "Detouring.h"
+#include <detours.h>
 
-using namespace std;
-int wmain(int argc, wchar_t** argv)
+/**
+ * The entry point of the CLI application.
+ * Uses the DetourCreateProcessWithDllEx function that first
+ * starts up the target proceess and modifies its import tables
+ */
+int wmain(int32_t argc, wchar_t** argv)
 {
-
 	if (argc < 3)
 	{
 		wprintf(L"Usage: injector [dll path] [process_name.exe]\n");
@@ -19,25 +19,26 @@ int wmain(int argc, wchar_t** argv)
 	}
 
 	STARTUPINFO si;
-    PROCESS_INFORMATION pi;
+	PROCESS_INFORMATION pi;
 
-    ZeroMemory(&si, sizeof(si));
-    ZeroMemory(&pi, sizeof(pi));
-    si.cb = sizeof(si);
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+	si.cb = sizeof(si);
 
 	size_t i;
 	char dll[MAX_PATH];
 	wcstombs_s(&i, dll, argv[1], MAX_PATH);
 
-    if (!DetourCreateProcessWithDllEx(argv[2], argv[3],
-        nullptr, nullptr, TRUE,
-        CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED,
+	if (!DetourCreateProcessWithDllEx(argv[2], argv[3],
+		nullptr, nullptr, TRUE,
+		CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED,
 		nullptr, nullptr, &si, &pi, dll, nullptr)) {
 		wprintf(L"\r\nERR %02X\r\n", GetLastError());
-    }
+	}
 
-    ResumeThread(pi.hThread);
-    CloseHandle(pi.hThread);
+	ResumeThread(pi.hThread);
+	CloseHandle(pi.hThread);
 	CloseHandle(pi.hProcess);
-}
 
+	return 0;
+}
