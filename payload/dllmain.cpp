d26d2e6d022883a9b09be3cd1541da576a38506f
mihaly044 <mihaly044@gmail.com>
2019-03-27 14:35:29 +0100

    Show a console window in debug builds

--- payload/dllmain.cpp
+++ payload/dllmain.cpp
@@ -1,6 +1,20 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 #include "payload.h"
+#include <cstdio>
+
+FILE* g_pCout;
+void OpenConsole()
+{
+	AllocConsole();
+	_wfreopen_s(&g_pCout, L"conout$", L"w", stdout);
+}
+
+void CloseConsole()
+{
+	FreeConsole();
+	fclose(g_pCout);
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule,
                       DWORD ul_reason_for_call,
@@ -14,10 +28,18 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+
+#ifdef _DEBUG
+		OpenConsole();
+#endif
+
 		DetourRestoreAfterWith();
 		if (!Hook())
 		{
 			MessageBox(nullptr, L"Detouring failed", L"Error", MB_OK);
+#ifdef _DEBUG
+			CloseConsole();
+#endif
 			return FALSE;
 		}
 		break;
@@ -27,8 +49,14 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 		if (!UnHook())
 		{
 			MessageBox(nullptr, L"Detouring failed", L"Error", MB_OK);
+#ifdef _DEBUG
+			CloseConsole();
+#endif
 			return FALSE;
 		}
+#ifdef _DEBUG
+		CloseConsole();
+#endif
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
